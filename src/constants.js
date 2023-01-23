26edc69fa0964e75795ff7122d830a3f86a198b1
Lucas Pereira <lpmelo10@gmail.com>
2023-01-23 18:29:12 -0300

    feat: adding environment to api routes

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const REACT_APP_LARAVEL_API_BASE_URL = "http://127.0.0.1:8000/api";
-export const REACT_APP_LARAVEL_API_ROUTE_GET_ALL = "/collaborators";
-export const REACT_APP_LARAVEL_API_ROUTE_PUT = "/collaborators/edit/";
-export const REACT_APP_LARAVEL_API_ROUTE_GET_BY_ID = "/collaborators/";
-export const REACT_APP_LARAVEL_API_POST = "/collaborators/new";
-export const REACT_APP_LARAVEL_API_DELETE = "/collaborators/delete/";
